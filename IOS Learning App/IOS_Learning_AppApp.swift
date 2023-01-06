73d1fec27d53d99cd914f959ba5e2bb8cd49c39f
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-01-06 07:10:02 +0530

    Initial Commit

---
+++ IOS Learning App/IOS_Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Learning_AppApp.swift
+//  IOS Learning App
+//
+//  Created by Mohammed Mubashir on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
