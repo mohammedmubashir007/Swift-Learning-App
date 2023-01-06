5c5df3d1fac75474b4314bdd3b4f2b228f8ef18a
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-01-06 08:10:15 +0530

    - add assets - add data files - add models - parsed json in content model - parsed html in content model

--- IOS Learning App/ViewModels/ContentModel.swift
+++ IOS Learning App/ViewModels/ContentModel.swift
@@ -9,4 +9,44 @@ import Foundation
 
 class ContentModel:ObservableObject {
     
+    
+    @Published var modules = [Module]()
+    var styleData : Data?
+    
+    
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            self.modules = modules
+            
+        }
+        catch{
+            print("Error in making data object")
+        }
+        
+        let parseStyle = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: parseStyle!)
+            self.styleData = styleData
+        }
+        catch {
+            
+            print("Error in parsing Style data")
+            
+        }
+        
+    }
+    
 }
